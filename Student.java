97f8adf1e77621c2973f6675a76aeefe0c035b3b
Seyyed Jalal Seyyedyazdi <seyyedyazdi@gmail.com>
2023-03-28 14:41:50 +0330

    Adding Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String name;
+    private String id;
+    private double avg;
+
+    public Student(String name, String id, double avg) {
+        this.name = name;
+        this.id = id;
+        this.name = name;
+    }
+
+		
+}
